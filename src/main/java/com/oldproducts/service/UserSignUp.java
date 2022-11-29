10ba13fbbea0cb1d9eedcb36a8c43e962e70a288
P Thirumal Prasad <119337624+porlathiru@users.noreply.github.com>
2022-11-29 10:24:46 +0530

    Update UserSignUp.java

--- src/main/java/com/oldproducts/service/UserSignUp.java
+++ src/main/java/com/oldproducts/service/UserSignUp.java
@@ -2,7 +2,7 @@ package com.oldproducts.service;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
